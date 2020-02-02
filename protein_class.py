8768b72a2de06505880795f1c186c9ccb1f09ddf
aqh2321 <51497458+aqh2321@users.noreply.github.com>
2020-02-01 17:30:27 -0800

    Update protein_class.py

--- protein_class.py
+++ protein_class.py
@@ -5,15 +5,15 @@ import os
 
 class Protein:
     # Initializer / Instance attributes
-    def __init__(self, name, filePath, energy = 0): #Default directory is current working directory
+    def __init__(self, filePath, name, energy = 0): #Default directory is current working directory
         self.filePath = filePath #1
         self.name = name #2
-        self.df = df #preprocessed pandas dataframe
         self.energy = energy #4
 
     # Returns the name of the protein and the file path associated with it
-    def description(self, filePath, name):
-        return "{}: {}".format(self.name, self.filePath)
+    # just print the object that been assigned with the class
+    def __repr__(self):
+        return "%s: %s" % (self.name, self.filePath)
 
     def setPath(self, filePath):
         try:
@@ -23,10 +23,15 @@ class Protein:
             return "Please pass a valid file path, {} does not exist".format(self.filePath)
 
     def rename(self, name):
-        self.name = input("Please enter a name: ")
+        self.name = input("Please enter names of models to compare in tuple: ")
 
-    def df(self, self.filePath, self.name):
-        self.df = frames(self.filePath, self.name)
+    def dataframe(self):
+        return frames(self.filePath, self.name)
         
 if __name__=='__main__':
-    
+    path = '/Users/mac/Desktop/'
+    name = 'model1.crd'
+    test = Protein(path, name)
+    print(test) #__repr__#
+    dftest = test.dataframe() #dataframe stored into variable in this form
+    print(dftest) #check output
