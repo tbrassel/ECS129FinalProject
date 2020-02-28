36248d32aa1fd8529cd90f10421bc6f881d1d024
tbrassel <37461300+tbrassel@users.noreply.github.com>
2020-02-28 13:21:50 -0800

    Add files via upload

--- dataframefunction.py
+++ dataframefunction.py
@@ -1,8 +1,10 @@
 #Module for processing and managing the dataframe
 import pandas as pd
+import os
+from functools import reduce
 
-def frames(filePath,name):
-    df = (pd.read_csv(filePath+name, skiprows = 2,delim_whitespace=True,header=None)).head(2288)
+def frames(filePath, name):
+    df = (pd.read_csv(filePath+name, skiprows = 2,delim_whitespace=True,header=None, index_col=None)).head(2288)
     df.columns = ['i','X','Y','Z','R','Epsilon','Sigma','Charge','ASP','Atm_name','Res_name','Res']
     df.Res = df['Res'].astype(int)
     realcolumns = ['i','Nexclude','E_list','NA1','NA2','NA3','NA4','NA5','NA6','NA7','NA8','NA9']
@@ -13,8 +15,45 @@ def frames(filePath,name):
     df3 = df3.drop(['i'],axis = 1)
     return df3
 
-if __name__ == '__main__':
-    name = ['model1.crd','model2.crd']
-    for i in range(len(name)):
-    	df = frames(name[i])
-    	pd.DataFrame.to_csv(df, path+name[i],index=False)
+def get_sanitized_input(path_to_dir):
+    while True:
+        try:
+            with os.path.exists(path_to_dir) and os.path.isdir(path_to_dir):
+                if not os.listdir(path_to_dir):
+                    return f"The directory entered appears to be empty, please add files for analysis."
+                    continue
+                else:
+                    to_process = os.listdir(path_to_dir)
+                    print(f"Processing {to_process} files... in folder ""{path_to_dir}.")
+                    #for path_to_dir, sub_dirs, names in os.walk(path_to_dir, topdown=True):
+        except IOError:
+            return f"Please pass a valid directory path, {path_to_dir} does not exist"
+            continue
+
+def testwalk(path_to_dir):
+    for root, dirs, files in os.walk("/home/tyler/ECS129FinalProjectInput/", topdown=True):
+            for name in files:
+                print(os.path.join(dirs[0], name))
+                
+def get_directory_structure(rootdir):
+    """
+    Creates a nested dictionary that represents the folder structure of rootdir
+    """
+    dir = {}
+    rootdir = rootdir.rstrip(os.sep)
+    start = rootdir.rfind(os.sep) + 1
+    types = list()
+    mod_files = list()
+    for path, dirs, files in os.walk(rootdir, topdown=True):
+        folders = path[start:].split(os.sep)
+        for i in range(0, len(files)):
+           split = os.path.splitext(files[i])
+           mod_files.append(split[0])
+           types.append(split[1])
+           
+        subdir = dict.fromkeys(files)
+        parent = reduce(dict.get, folders[:-1], dir)
+        parent[folders[-1]] = subdir
+    return dir
+
+
--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 #Main method, our actual executable file 
-from dataframefunction import frames
+from dataframefunction import frames, get_directory_structure
 from protein_class import Protein
 import ecalc as e
 import pandas as pd
 import numpy as np
 import os
+import re
 
 if __name__=='__main__':
-    path = "/home/tyler/ECS129FinalProjectInput/"
-    name = "model1.crd"
-    protein1 = Protein(filePath=path, name=name)
-    print(protein1) #__repr__#
-    dftest = protein1.dataframe() #dataframe stored into variable in this form
-    print(dftest) #check output
-    protein1_energy = e.energy(protein1) 
-    print(f"{name} has an energy value of {protein1_energy}")
+   path = "/home/tyler/ECS129FinalProjectInput/Protein2/"
+    namelist = list()
+    with os.scandir(path) as entries:
+        for entry in entries:
+            #Use list unpacking to create the list of filenames in the subdirectory
+            namelist = [*namelist, entry.name]
+    #Sort the filenames in ascending order
+    namelist.sort(key=lambda f: int(re.sub('\D', '', f)))
+    
+    for i in range(0, len(namelist)):
+       name = namelist[i]
+       protein = Protein(filePath=path, name=name)
+       protein_energy = e.energy(protein) 
+       print(f"{name} has an energy value of {protein_energy}")
+
+    # Testing functions:
+   #protein1 = Protein(filePath=path, name = "model01.crd")
+   #print(protein1) #__repr__#
+   #dftest = protein1.dataframe() #dataframe stored into variable in this form
+   #print(dftest) #check output
+    # dir = get_directory_structure(path)
     
